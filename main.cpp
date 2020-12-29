39b55558c7c33633e8d1cd67a5b1579f790044b9
/mrk7711 <mohammadreza7711@yahoo.com>
2020-12-30 01:29:13 +0330

    Now We have menu in our program!

--- main.cpp
+++ main.cpp
@@ -2,51 +2,181 @@
 #include "puzzle.h"
 #include "bfs.h"
 #include "dfs.h"
+#include <ctime>
+void show(int *  &);
 int main()
 {
-    int * p={new int[9]};
-    p[0]=1;
-    p[1]=2;
-    p[2]=3;
-    p[3]=4;
-    p[4]=5;
-    p[5]=0;
-    p[6]=6;
-    p[7]=7;
-    p[8]=8;
-    Node a(p);
-
-    
-    Dfs x;
-    std::vector<Node*> t=x.DFS(&a);
-    if(t.size()>0)
+    std::cout<<"Welcome to puzzle!"<<std::endl;
+    int c{};
+    int ch;
+    char y='y';
+    while(y=='y')
     {
-        for(size_t i{};i<t.size();i++)
+        std::cout<<"If you want to enter your puzzle please select 1:\n";
+        std::cout<<"If you want to see the solution of random puzzle please select 2:\n";
+        std::cin>>ch;
+        switch(ch)
         {
-            t[i]->show_puzzle();
-            std::cout<<"$$$$$$"<<std::endl;
+            case 1:
+            {
+                int * p={new int[9]};
+                p[0]=0;
+                p[1]=0;
+                p[2]=0;
+                p[3]=0;
+                p[4]=0;
+                p[5]=0;
+                p[6]=0;
+                p[7]=0;
+                p[8]=0;
+                show(p);
+                int c;
+                for(size_t i{};i<9;i++)
+                {
+                    std::cout<<"Please enter the number"<<i<<": \n";
+                    std::cin>>c;
+                    p[i]=c;
+                    show(p);
+                }
+            
+            std::cout<<"Select the Algorithm?\n";
+            std::cout<<"BFS?-->1\n";
+            std::cout<<"DFS?-->2\n";
+            std::cin>>c;
+            switch(c)
+            {
+                case 1:
+                {
+                    Node a(p);
+                    Bfs y;
+                    std::vector<Node*> u=y.BFS(&a);
+                    if(u.size()>0)
+                    {
+                        for(size_t i{};i<u.size();i++)
+                        {
+                            u[i]->show_puzzle();
+                            std::cout<<"$$$$$$"<<std::endl;
+                        }
+                    }
+                    else 
+                    {
+                        std::cout<<"No Path!";
+                    }
+                    std::cout<<u.size();
+                    break;
+                }
+                case 2:
+                {
+                    Node a(p);
+                    Dfs x;
+                    std::vector<Node*> t=x.DFS(&a);
+                    if(t.size()>0)
+                    {
+                        for(size_t i{};i<t.size();i++)
+                        {
+                            t[i]->show_puzzle();
+                            std::cout<<"$$$$$$"<<std::endl;
+                        }
+                    }
+                    else 
+                    {
+                        std::cout<<"No Path!";
+                    }
+                    std::cout<<t.size();
+                    break;
+                }
+            }
+            break;
+            }
+            case 2:
+            {
+                srand(time(0));
+                int *p={new int[9]};
+                size_t i=0;
+                while(i<9)
+                {
+                    p[i]=(rand()%9);
+                    for(size_t j{};j<i;j++)
+                    {
+                        if(p[i]==p[j])
+                        {
+                            i--;
+                            break;
+                        }
+                    } 
+                    i++;
+                }
+                show(p);
+                            std::cout<<"Select the Algorithm?\n";
+            std::cout<<"BFS?-->1\n";
+            std::cout<<"DFS?-->2\n";
+            std::cin>>c;
+            switch(c)
+            {
+                case 1:
+                {
+                    Node a(p);
+                    Bfs y;
+                    std::vector<Node*> u=y.BFS(&a);
+                    if(u.size()>0)
+                    {
+                        for(size_t i{};i<u.size();i++)
+                        {
+                            u[i]->show_puzzle();
+                            std::cout<<"$$$$$$"<<std::endl;
+                        }
+                    }
+                    else 
+                    {
+                        std::cout<<"No Path!";
+                    }
+                    std::cout<<u.size();
+                    break;
+                }
+                case 2:
+                {
+                    Node a(p);
+                    Dfs x;
+                    std::vector<Node*> t=x.DFS(&a);
+                    if(t.size()>0)
+                    {
+                        for(size_t i{};i<t.size();i++)
+                        {
+                            t[i]->show_puzzle();
+                            std::cout<<"$$$$$$"<<std::endl;
+                        }
+                    }
+                    else 
+                    {
+                        std::cout<<"No Path!";
+                    }
+                    std::cout<<t.size();
+                    break;
+                }
+            }
+                break;
+            }
+        }
+        std::cout<<"Do you want to continue?";
+        std::cin>>y;
+        if(y!='y')
+        {
+            break;
         }
     }
-    else 
-    {
-        std::cout<<"No Path!";
-    }
-    // std::cout<<t.size();
-    // Bfs y;
-    // std::vector<Node*> u=y.BFS(&a);
-    // if(u.size()>0)
-    // {
-    //     for(size_t i{};i<u.size();i++)
-    //     {
-    //         u[i]->show_puzzle();
-    //         std::cout<<"$$$$$$"<<std::endl;
-    //     }
-    // }
-    // else 
-    // {
-    //     std::cout<<"No Path!";
-    // }
-    // // std::cout<<t.size();
-    return 0;
+return 0;
+}
 
-} 
+void show(int * &p)
+{
+    int x{};
+    for (size_t i{};i<3;i++)
+    {
+        for (size_t j{};j<3;j++)
+        {
+            std::cout<<p[x]<<" ";
+            x++;
+        }
+        std::cout<<std::endl;
+    }
+}
